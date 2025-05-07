0ca109563889734fbacd2c976f9a5f5afba64e78
Taha Qamar <tahaqamar10@gmail.com>
2025-05-06 23:54:41 -0700

    updated sounds

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,56 @@
-// expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector("#expose img");
+  const audio = document.querySelector("audio");
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector("#volume-controls img");
+  const playButton = document.querySelector("button");
+  const jsConfetti = new JSConfetti();
+
+  const hornAssets = {
+    'air-horn': {
+      img: 'assets/images/air-horn.svg',
+      audio: 'assets/audio/air-horn.mp3'
+    },
+    'car-horn': {
+      img: 'assets/images/car-horn.svg',
+      audio: 'assets/audio/car-horn.mp3'
+    },
+    'party-horn': {
+      img: 'assets/images/party-horn.svg',
+      audio: 'assets/audio/party-horn.mp3'
+    }
+  };
+
+  hornSelect.addEventListener('change', () => {
+    const selected = hornSelect.value;
+    if (hornAssets[selected]) {
+      hornImage.src = hornAssets[selected].img;
+      audio.src = hornAssets[selected].audio;
+    }
+  });
+
+  volumeSlider.addEventListener('input', () => {
+    const volume = volumeSlider.value;
+    audio.volume = volume / 100;
+
+    if (volume == 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+    } else if (volume < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+    } else if (volume < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+    } else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+    }
+  });
+
+  playButton.addEventListener('click', () => {
+    audio.play();
+    if (hornSelect.value === 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+  });
 }
